acc167ce94cdd59cea3b2dc9d72f1c269930c1fd
Pramudya <pramudyap333@gmail.com>
2024-07-19 22:07:42 +0700

    update view

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -7,6 +7,17 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+  <style>
+    .image-preview {
+        width: 100%;
+        height: auto;
+        max-height: 200px;
+        margin-top: 10px;
+        border: 1px solid #ddd;
+        padding: 5px;
+    }
+</style>
+
 </head>
 <body>
     <div class="d-flex justify-content-center">
@@ -40,7 +51,10 @@
                 @csrf
                 <div class="form-group">
                     <label for="image">Choose an image:</label>
-                    <input type="file" name="image" class="form-control" id="image">
+                    <input type="file" name="image" class="form-control" id="image" accept="image/*" onchange="previewImage(event)">
+                </div>
+                <div class="form-group">
+                    <img id="image-preview" class="image-preview" src="https://via.placeholder.com/200" alt="Image Preview">
                 </div>
                 <div class="form-group">
                     <button type="submit" class="btn btn-primary" name="action" value="breed">breed detect</button>
@@ -53,5 +67,17 @@
         </div>
     </div>
 
-</body>x
+    <script>
+        function previewImage(event) {
+            var reader = new FileReader();
+            reader.onload = function(){
+                var output = document.getElementById('image-preview');
+                output.src = reader.result;
+                output.style.display = 'block';
+            }
+            reader.readAsDataURL(event.target.files[0]);
+        }
+    </script>
+
+</body>
 </html>
